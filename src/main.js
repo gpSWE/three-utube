8dcb0da9cf518ead65ab20fe7d978445ab7b74a7
Muhammadxon Najimov <129692531+gpSWE@users.noreply.github.com>
2024-08-27 12:34:29 +0500

    Add controls

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three"
+import { MapControls } from "three/addons/controls/MapControls"
 
 // Setup
 
@@ -15,6 +16,13 @@ const renderer = new THREE.WebGLRenderer( { canvas } )
 renderer.setPixelRatio( window.devicePixelRatio )
 renderer.setSize( window.innerWidth, window.innerHeight )
 
+const controls = new MapControls( camera, canvas )
+controls.minDistance = 10
+controls.maxDistance = 100
+controls.maxPolarAngle = Math.PI / 2 - 0.25
+controls.maxTargetRadius = 10
+controls.enableDamping = true
+
 window.addEventListener( "resize", () => {
 
 	camera.aspect = window.innerWidth / window.innerHeight
@@ -23,11 +31,17 @@ window.addEventListener( "resize", () => {
 	renderer.setSize( window.innerWidth, window.innerHeight )
 } )
 
-renderer.setAnimationLoop( () => renderer.render( scene, camera ) )
+renderer.setAnimationLoop( () => {
+
+	controls.update()
+
+	renderer.render( scene, camera )
+} )
 
 //
 
 scene.add( new THREE.AxesHelper( 200 ) )
+scene.add( new THREE.GridHelper( 20, 20, 0x808080, 0x404040 ) )
 
 {
 	const geometry = new THREE.BoxGeometry()
