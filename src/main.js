fa2e7543432b06d1b062e97b2bbf6b8008f163ad
Muhammadxon Najimov <129692531+gpSWE@users.noreply.github.com>
2024-08-27 12:14:59 +0500

    Add scene, camera, renderer

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 import * as THREE from "three"
 
-console.log( THREE.REVISION )
+const canvas = document.getElementById( "webgl" )
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera()
+
+const renderer = new THREE.WebGLRenderer( { canvas } )
+
+renderer.setAnimationLoop( () => renderer.render( scene, camera ) )
