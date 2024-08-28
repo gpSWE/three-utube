c098ff50e91a833f5038cf1878e9e7523984f1fd
Muhammadxon Najimov <129692531+gpSWE@users.noreply.github.com>
2024-08-28 15:51:24 +0500

    Refactor the source code

--- src/main.js
+++ src/main.js
@@ -1,42 +1,7 @@
 import * as THREE from "three"
-import { MapControls } from "three/addons/controls/MapControls"
+import { setupScene } from "./setup-scene"
 
-// Setup
-
-const canvas = document.getElementById( "webgl" )
-
-const scene = new THREE.Scene()
-scene.background = new THREE.Color( 0x202020 )
-
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 200 )
-camera.position.set( 10, 10, 10 )
-camera.lookAt( 0, 0, 0 )
-
-const renderer = new THREE.WebGLRenderer( { canvas } )
-renderer.setPixelRatio( window.devicePixelRatio )
-renderer.setSize( window.innerWidth, window.innerHeight )
-
-const controls = new MapControls( camera, canvas )
-controls.minDistance = 10
-controls.maxDistance = 100
-controls.maxPolarAngle = Math.PI / 2 - 0.25
-controls.maxTargetRadius = 10
-controls.enableDamping = true
-
-window.addEventListener( "resize", () => {
-
-	camera.aspect = window.innerWidth / window.innerHeight
-	camera.updateProjectionMatrix()
-
-	renderer.setSize( window.innerWidth, window.innerHeight )
-} )
-
-renderer.setAnimationLoop( () => {
-
-	controls.update()
-
-	renderer.render( scene, camera )
-} )
+const { scene } = setupScene( { canvas: document.getElementById( "webgl" ) } )
 
 //
 
@@ -49,8 +14,4 @@ scene.add( new THREE.GridHelper( 20, 20, 0x808080, 0x404040 ) )
 	const object = new THREE.Mesh( geometry, material )
 	scene.add( object )
 }
-
-
-
-
 
---
+++ src/setup-scene.js
@@ -0,0 +1,45 @@
+import * as THREE from "three"
+import { MapControls } from "three/addons/controls/MapControls"
+
+export function setupScene( { canvas } ) {
+
+	// Scene
+	const scene = new THREE.Scene()
+	scene.background = new THREE.Color( 0x202020 )
+
+	// Camera
+	const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 200 )
+	camera.position.set( 10, 10, 10 )
+
+	// Renderer
+	const renderer = new THREE.WebGLRenderer( { canvas } )
+	renderer.setPixelRatio( window.devicePixelRatio )
+	renderer.setSize( window.innerWidth, window.innerHeight )
+
+	// Controls
+	const controls = new MapControls( camera, canvas )
+	controls.enableDamping = true
+
+	// onResize
+	window.addEventListener( "resize", () => {
+
+		camera.aspect = window.innerWidth / window.innerHeight
+		camera.updateProjectionMatrix()
+
+		renderer.setSize( window.innerWidth, window.innerHeight )
+	} )
+
+	renderer.setAnimationLoop( () => {
+
+		controls.update()
+
+		renderer.render( scene, camera )
+	} )
+
+	return {
+		scene,
+		camera,
+		renderer,
+		controls,
+	}
+}
