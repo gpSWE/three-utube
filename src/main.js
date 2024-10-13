6182ba9f655d63b3fe5ca4505e5826df90dc567a
Muhammadxon Najimov <129692531+gpSWE@users.noreply.github.com>
2024-10-13 14:42:16 +0500

    Float32BufferAttribute

--- src/main.js
+++ src/main.js
@@ -12,38 +12,45 @@ const textureLoader = new THREE.TextureLoader().setPath( "/assets/textures" )
 
 const uvChecker = textureLoader.load( "/uvChecker.png" )
 uvChecker.colorSpace = THREE.SRGBColorSpace
+uvChecker.wrapS = uvChecker.wrapT = THREE.RepeatWrapping
+// uvChecker.repeat.set( 1, 1 )
+// uvChecker.offset.set( 0, 0 )
+
+// const t1 = textureLoader.load( "/bricks/color.jpg" )
+// t1.wrapS = THREE.RepeatWrapping
+// t1.wrapT = THREE.RepeatWrapping
+// t1.colorSpace = THREE.SRGBColorSpace
+// t1.repeat.set( 2, 2 )
 
 {
 	const geometry = new THREE.PlaneGeometry( 5, 5, 1, 1 ).rotateX( - Math.PI / 2 )
 	const material = new THREE.MeshBasicMaterial( { wireframe: !true, map: uvChecker } )
 	const object = new THREE.Mesh( geometry, material )
 	// scene.add( object )
-
-	console.log( geometry.attributes.position )
 }
 
 {
-	const positionArray = new Float32Array( [
-		- 2.5, 0, + 2.5,
-		+ 2.5, 0, + 2.5,
-		+ 2.5, 0, - 2.5,
-		- 2.5, 0, - 2.5,
-	] )
-
-	// const positionAttribute = new THREE.BufferAttribute( positionArray, 3 )
-	const positionAttribute = new THREE.Float32BufferAttribute( [
+	const position = new THREE.Float32BufferAttribute( [
 		- 2.5, 0, + 2.5,
 		+ 2.5, 0, + 2.5,
 		+ 2.5, 0, - 2.5,
 		- 2.5, 0, - 2.5,
 	], 3 )
 
+	const uv = new THREE.Float32BufferAttribute( [
+		0, 0,
+		1, 0,
+		1, 1,
+		0, 1,
+	], 2 )
+
 	const geometry = new THREE.BufferGeometry()
-	geometry.setAttribute( "position", positionAttribute )
+	geometry.setAttribute( "position", position )
+	geometry.setAttribute( "uv", uv )
 	geometry.setIndex( [ 0, 1, 2, 2, 3, 0 ] )
 	const material = new THREE.MeshBasicMaterial( { wireframe: !true, map: uvChecker } )
 	const object = new THREE.Mesh( geometry, material )
 	scene.add( object )
 
-	// console.log( geometry.attributes )
+	console.log( geometry.attributes )
 }
